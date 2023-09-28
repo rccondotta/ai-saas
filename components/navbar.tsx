022eae7c8e3b6c16b0bbc3f41f0a03c2b1549d20
unknown <rccondotta@hotmail.com>
2023-09-28 18:40:05 -0400

    Adding Sidebar and Dashboard Components

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,19 +1,16 @@
 import { UserButton } from "@clerk/nextjs";
-import { Menu } from "lucide-react";
 
-import { Button } from "@/components/ui/button";
+import MobileSidebar from "@/components/mobile-sidebar";
 
 const Navbar = () => {
-    return (
-        <div className="flex items-center p-4">
-            <Button variant="ghost" size="icon" className="md:hidden">
-                <Menu />
-            </Button>
-            <div className="flex w-full justify-end">
-                <UserButton afterSignOutUrl="/"/>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="flex items-center p-4">
+      <MobileSidebar />
+      <div className="flex w-full justify-end">
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
